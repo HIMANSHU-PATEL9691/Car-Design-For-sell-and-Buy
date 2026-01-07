bede8685eacdb9338ecd812cc3f73814fd89e068
Himanshu Patel <himanshupatel9691@gmail.com>
2026-01-07 17:24:04 +0530

    update main

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { ThemeProvider } from "next-themes";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem={false}
+    >
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
